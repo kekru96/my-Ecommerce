0a475c65c9b18a3ddaa3d1f918b885dbb640f886
kekru96 <kevinkru96@hotmail.com>
2023-03-16 20:20:16 -0300

    Primer commit

---
+++ src/components/navbar.js
@@ -0,0 +1,18 @@
+
+import "./navbar.css";
+
+export const NavBar = ()=>{
+
+    return <div className="container">
+        <ul>
+            <li><a href="a">Inicio</a></li>
+            <li><a href="a">Comprar</a></li>
+            <li><a href="a">Contactanos</a></li>    
+        </ul>
+        <ul>
+            <li>
+                <a href="a">Carrito</a>
+            </li>
+        </ul>
+    </div>
+}
